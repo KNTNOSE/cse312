b3406e6f9279ed25484a72e36877587ed0fdb338
KNTNOSE <work1003.n@gmail.com>
2023-09-28 13:33:54 -0400

    mongo

--- server.py
+++ server.py
@@ -1,18 +1,103 @@
 import socketserver
 import sys
 from util.request import Request
+import os
+import html
+import json
+
+import pymongo
+from pymongo import MongoClient
+
+# Establish a connection to the MongoDB
+mongo_client = MongoClient("mongo")
+db = mongo_client["cse312"]
+chat_collection = db["chat"]
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
+        response = ""
         received_data = self.request.recv(2048)
-        print(self.client_address)
-        print("--- received data ---")
-        print(received_data)
-        print("--- end of data ---\n\n")
+        # print(self.client_address)
+        # print("--- received data ---")
+        # print(received_data)
+        # print("--- end of data ---\n\n")
         request = Request(received_data)
 
         # TODO: Parse the HTTP request and use self.request.sendall(response) to send your response
+        if request.path == '/':
+            # リクエストが / の場合、public/index.html の内容を返す
+            file_path = "public/index.html"
+            
+            if os.path.exists(file_path) and os.path.isfile(file_path):
+                with open(file_path, "rb") as file:
+                    content = file.read()
+                
+                content_length = len(content)
+                content_length_header = f"Content-Length: {content_length}\r\n"
+                response = (
+                    b"HTTP/1.1 200 OK\r\n"
+                    + content_length_header.encode("utf-8")
+                    + b"Content-Type: text/html\r\n\r\n"
+                    + content
+                )
+
+        elif request.path.startswith("/image/"):
+            # リクエストが /image/ から始まる場合
+            image_file_name = request.path[len("/image/"):]
+            file_path = f"public/image/{image_file_name}"
+
+            if os.path.exists(file_path) and os.path.isfile(file_path):
+                with open(file_path, "rb") as file:
+                    content = file.read()
+                
+                content_length = len(content)
+                content_type_header = b"Content-Type: image/jpeg\r\n"  # JPEG画像の場合
+
+                response = (
+                    b"HTTP/1.1 200 OK\r\n"
+                    + f"Content-Length: {content_length}\r\n".encode("utf-8")
+                    + content_type_header
+                    + b"\r\n"
+                    + content
+                )
+
+        elif request.path == "/chat-message" and request.method == "POST":
+            data = request.body.decode("utf-8")
+            message = html.escape(data.split('=')[1])
+            username = "Guest"
+            
+            # メッセージをデータベースに保存
+            chat_collection.insert_one({
+                "message": message,
+                "username": username
+            })
+
+            response = b"HTTP/1.1 200 OK\r\nContent-Length: 16\r\n\r\nMessage received"
+
+        elif request.path == "/chat-history" and request.method == "GET":
+            
+            messages = chat_collection.find()
+            chat_history = [{
+                "message": msg["message"],
+                "username": msg["username"],
+                "id": str(msg["_id"])
+            } for msg in messages]
+            
+            chat_data = json.dumps(chat_history)
+            response = (
+                b"HTTP/1.1 200 OK\r\n"
+                + f"Content-Length: {len(chat_data)}\r\n".encode('utf-8')
+                + b"Content-Type: application/json\r\n\r\n"
+                + chat_data.encode('utf-8')
+            )
+
+        else:
+            # ファイルが存在しない場合、404 エラーを返す
+            response = b"HTTP/1.1 404 Not Found\r\nContent-Length: 6\r\n\r\ninside"
+
+        self.request.sendall(response)
+
 
 
 def main():
@@ -29,6 +114,9 @@ def main():
 
     server.serve_forever()
 
+
+if __name__ == "__main__":
+    main()
 
 if __name__ == "__main__":
     main()
--- util/request.py
+++ util/request.py
@@ -3,8 +3,74 @@ class Request:
     def __init__(self, request: bytes):
         # TODO: parse the bytes of the request and populate the following instance variables
 
+        self.request_str = request.decode("utf-8")
+
         self.body = b""
         self.method = ""
         self.path = ""
         self.http_version = ""
         self.headers = {}
+        
+        self.parse_headers()
+        self.parse_body(request)
+        self.cookies = self._parse_cookies()
+
+
+    def parse_headers(self):
+        headers_section = self.request_str.split('\r\n\r\n', 1)[0]
+        request_line = headers_section.split('\r\n')[0]
+        header_list = headers_section.split('\r\n')[1:]
+
+        parts = request_line.split(' ')
+        if len(parts) >= 2:
+            self.method = parts[0]
+            self.path = parts[1]
+            self.http_version=parts[2]
+        
+
+        for header in header_list:
+            header_name, header_value = header.split(': ', 1)
+
+            if header_name in self.headers:
+                if not isinstance(self.headers[header_name], list):
+                    self.headers[header_name] = [self.headers[header_name]]
+                self.headers[header_name].append(header_value)
+            else:
+                self.headers[header_name] = header_value
+
+    def parse_body(self, request):
+        if '\r\n\r\n' in self.request_str:
+            body_section = self.request_str.split('\r\n\r\n', 1)[1]
+            self.body = body_section.encode("utf-8")
+        else:
+            # リクエストが正しくない場合、エラーレスポンスを生成
+            self.body = b"Bad Request"
+
+
+    def set_content_length(self):
+        content_length = len(self.body)
+        self.headers["Content-Length"] = str(content_length)
+
+
+    def _parse_cookies(self):
+        """
+        HTTPヘッダからクッキーを解析して辞書で返します。
+        """
+        cookies = {}
+        cookie_header = self.headers.get('Cookie')
+        if cookie_header:
+            cookie_pairs = cookie_header.split(';')
+            for pair in cookie_pairs:
+                key, value = pair.strip().split('=', 1)
+                cookies[key] = value
+        return cookies
+
+    def get_cookie(self, key, default=None):
+        """
+        指定されたキーのクッキーの値を返します。クッキーが存在しない場合はdefaultを返します。
+        """
+        return self.cookies.get(key, default)
+
+
+
+
